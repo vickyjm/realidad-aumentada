dd21467e1e95ca9603c7856c4ea1d736543d13bc
jemd93 <jemd311@gmail.com>
2017-02-11 16:10:15 -0400

    Pusheando algunas cosas de info

--- ARP/Assets/Scripts/BrainParts.cs
+++ ARP/Assets/Scripts/BrainParts.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class BrainParts : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject infoText;
+    public GameObject infoTitle;
+    // Use this for initialization
+    void Start () {
+        infoText = GameObject.FindWithTag("infoText");
+        infoTitle = GameObject.FindWithTag("infoTitle");
+        infoText.GetComponent<TextMesh>().text = "i";
+        infoTitle.GetComponent<TextMesh>().text = "";
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
+    void OnMarkerTracked(ARMarker marker)
+    {
+        if (marker.Tag == "cere")
+            infoTitle.GetComponent<TextMesh>().text = "Cerebelo";
+        else if (marker.Tag == "kanji")
+            infoTitle.GetComponent<TextMesh>().text = "Parietal";
+        else if (marker.Tag == "fron")
+            infoTitle.GetComponent<TextMesh>().text = "Frontal";
+        else if (marker.Tag == "occ")
+            infoTitle.GetComponent<TextMesh>().text = "Occipital";
+        else if (marker.Tag == "temp")
+            infoTitle.GetComponent<TextMesh>().text = "Temporal";
+        else if (marker.Tag == "unai")
+            infoTitle.GetComponent<TextMesh>().text = "Stem";
+    }
+
     void OnMarkerLost(ARMarker marker)
     {
+        infoText.GetComponent<TextMesh>().text = "i";
+        infoTitle.GetComponent<TextMesh>().text = "";
         Debug.Log(marker.name + " Se fue");
         //this.transform.position = this.transform.parent.parent.position;
     }
