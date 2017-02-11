3d5f792ec818e723f115a1d94b574bc0c308b6cc
jemd93 <jemd311@gmail.com>
2017-02-11 16:39:02 -0400

    Colocadas parte de las informaciones

--- ARP/Assets/Scripts/BrainParts.cs
+++ ARP/Assets/Scripts/BrainParts.cs
@@ -10,8 +10,8 @@ public class BrainParts : MonoBehaviour {
     void Start () {
         infoText = GameObject.FindWithTag("infoText");
         infoTitle = GameObject.FindWithTag("infoTitle");
-        infoText.GetComponent<TextMesh>().text = "i";
-        infoTitle.GetComponent<TextMesh>().text = "";
+        infoText.GetComponent<TextMesh>().text = "Coloca otra marca en la pantalla \npara ver informacion de las \ndistintas partes del cerebro!";
+        infoTitle.GetComponent<TextMesh>().text = "Informacion";
     }
 	
 	// Update is called once per frame
@@ -22,23 +22,41 @@ public class BrainParts : MonoBehaviour {
     void OnMarkerTracked(ARMarker marker)
     {
         if (marker.Tag == "cere")
+        {
             infoTitle.GetComponent<TextMesh>().text = "Cerebelo";
+            infoText.GetComponent<TextMesh>().text = "El cerebelo es la parte del \ncerebro encargada de...";
+        }
         else if (marker.Tag == "kanji")
+        {
             infoTitle.GetComponent<TextMesh>().text = "Parietal";
+            infoText.GetComponent<TextMesh>().text = "El lobulo parietal es la parte del \ncerebro encargada de...";
+        }
         else if (marker.Tag == "fron")
+        {
             infoTitle.GetComponent<TextMesh>().text = "Frontal";
+            infoText.GetComponent<TextMesh>().text = "El lobulo frontal es la parte del \ncerebro encargada de...";
+        }
         else if (marker.Tag == "occ")
+        {
             infoTitle.GetComponent<TextMesh>().text = "Occipital";
+            infoText.GetComponent<TextMesh>().text = "El lobulo occipital es la parte del \ncerebro encargada de...";
+        }
         else if (marker.Tag == "temp")
+        {
             infoTitle.GetComponent<TextMesh>().text = "Temporal";
+            infoText.GetComponent<TextMesh>().text = "El lobulo temporal es la parte del \ncerebro encargada de...";
+        }
         else if (marker.Tag == "unai")
-            infoTitle.GetComponent<TextMesh>().text = "Stem";
+        {
+            infoTitle.GetComponent<TextMesh>().text = "Tronco del encefalo";
+            infoText.GetComponent<TextMesh>().text = "El tronco del encefalo es la parte del \ncerebro encargada de...";
+        }
     }
 
     void OnMarkerLost(ARMarker marker)
     {
-        //infoText.GetComponent<TextMesh>().text = "i";
-        infoTitle.GetComponent<TextMesh>().text = "";
+        infoText.GetComponent<TextMesh>().text = "Coloca otra marca en la pantalla \npara ver informacion de las \ndistintas partes del cerebro!";
+        infoTitle.GetComponent<TextMesh>().text = "Informacion";
         Debug.Log(marker.name + " Se fue");
         //this.transform.position = this.transform.parent.parent.position;
     }
